9b4c0a785248aeac675f87d77ebd9e61cf7e54f7
Marco Rodriguez <mar_zidane@hotmail.com>
2023-07-13 11:09:54 -0600

    get mac address method finished

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('inicio');
+const os = require('os');
+
+function getMacAddress() {
+    try {
+        const networInterfaces = os.networkInterfaces();
+
+        for (const key in networInterfaces) {
+            const macAddresses = networInterfaces[key].map((networkInterface) => {
+                return networkInterface.mac;
+            });
+
+            const filteredMacAddresses = macAddresses.filter((macAddress) => macAddress !== '00:00:00:00:00:00');
+
+            if (filteredMacAddresses.length) {
+                return filteredMacAddresses[0];
+            }
+        }
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+console.log(getMacAddress());
